cf2df173dbf8acb38f5fb49a3737b8c53fc1a8cf
vatsalsin <vatsal.eliot@gmail.com>
2019-05-09 00:52:36 +0530

    add trial code

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {dialogflow} = require('actions-on-google');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+const server = express();
+const assistant = dialogflow();
+		
+server.set('port', process.env.PORT || 5000);
+server.use(bodyParser.json({type: 'application/json'}));
+
+assistant.intent('helloWorld', conv => {
+	let name = conv.parameters.any;
+	console.log('Hello, welcome ' + name);
+	conv.ask('Hello, welcome ' + name);
+});
+
+server.post('/webhook', assistant);
+
+server.listen(server.get('port'), function () {
+	console.log('Express server started on port', server.get('port'));
+});
