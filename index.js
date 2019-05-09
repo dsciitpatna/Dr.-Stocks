b356f32df6554ee1fb78f4f8076ce709b07dbc2b
vatsalsin <vatsal.eliot@gmail.com>
2019-05-10 04:50:03 +0530

    most basic prototype

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ assistant.intent('Opening Price', conv => {
 
 	return new Promise((resolve, reject) => {
 	  let name = conv.parameters.any;
-	  const priceType = conv.parameters['price-type'];
+	  const query = conv.parameters['price-type'];
       axios.get('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='+name+'&apikey='+apiKey)
       .then( (result) => {
 	      if (result.status == 200) {
@@ -26,27 +26,33 @@ assistant.intent('Opening Price', conv => {
 		    let symbol = bestMatch["1. symbol"];
 		    let curr = bestMatch["8. currency"];
 		    axios.get('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='+symbol+'&apikey='+apiKey)
-		    .then((res) => {
+		    .then((res) => { 
 		    	console.log(res)
-			    console.log('Hello, welcome ' + name + priceType+symbol+curr+res.data["Global Quote"]["08. previous close"]);
-			    if(priceType === 'closing price'){
-			   		resolve('Closing Price of' + name +" is "+res.data["Global Quote"]["08. previous close"]+" "+curr); 	
+			    console.log('Hello, welcome ' + name + query+symbol+curr+res.data["Global Quote"]["08. previous close"]);
+			    if(query === 'closing price'){
+			   		resolve('Closing Price of ' + name + " is " + res.data["Global Quote"]["08. previous close"] + " " + curr); 	
 				}
-				else if(priceType === 'opening price'){
-					resolve('Closing Price of' + name +" is "+res.data["Global Quote"]["08. previous close"]+" "+curr);
+				else if(query === 'opening price'){
+					resolve('Opening Price of ' + name + " is " + res.data["Global Quote"]["02. open"] + " " + curr);
 				}
-				else if(priceType === 'high price'){
-					resolve('Closing Price of' + name +" is "+res.data["Global Quote"]["08. previous close"]+" "+curr);
+				else if(query === 'high price'){
+					resolve('High of ' + name + " is " + res.data["Global Quote"]["03. high"] + " " + curr);
 				}
-				else if(priceType === 'low price'){
-					resolve('Closing Price of' + name +" is "+res.data["Global Quote"]["08. previous close"]+" "+curr);
+				else if(query === 'low price'){
+					resolve('Low of ' + name + " is " + res.data["Global Quote"]["04. low"] + " " +curr);
+				}
+				else if(query === 'current price'){
+					resolve('Current trading price of ' + name + " is " + res.data["Global Quote"]["04. low"] + ", with a change of " + res.data["Global Quote"]["10. change percent"] + " or " + res.data["Global Quote"]["09. change"] + " " + curr);
+				}
+				else if(query === 'volume'){
+					resolve('Volume of ' + name + " is " + res.data["Global Quote"]["06. volume"] + " " + curr);
 				}
 			})
 			.catch((err) => {
 				reject(err);
 			});
 	       
-      }
+      	  }
 	  })
 	  .catch((error) => {
 	  	reject(error);
@@ -54,10 +60,10 @@ assistant.intent('Opening Price', conv => {
 	})
 	.then(result => {
 	    conv.ask(result);
-	 })
+	})
 	.catch(error => {
 	    conv.close(error);
-	  });
+	});
 });
 
 server.post('/webhook', assistant);
